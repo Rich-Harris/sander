3b5598006fd3ce7411f51c20b5baa8514fddebe3
Rich-Harris <richard.a.harris@gmail.com>
2015-10-22 23:13:29 -0400

    add es6 build

--- gobblefile.js
+++ gobblefile.js
@@ -1,9 +1,21 @@
 var gobble = require( 'gobble' );
 
-module.exports = gobble( 'src' )
-	.transform( 'babel' )
-	.transform( 'esperanto-bundle', {
-		entry: 'sander',
-		type: 'cjs',
-		strict: true
-	});
+module.exports = gobble([
+	// CommonJS build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'sander.js',
+		dest: 'sander.cjs.js',
+		format: 'cjs',
+		external: [ 'graceful-fs', 'es6-promise', 'path', 'mkdirp', 'fs', 'rimraf' ],
+		sourceMap: true
+	}),
+
+	// ES6 build
+	gobble( 'src' ).transform( 'rollup-babel', {
+		entry: 'sander.js',
+		dest: 'sander.es6.js',
+		format: 'es6',
+		external: [ 'graceful-fs', 'es6-promise', 'path', 'mkdirp', 'fs', 'rimraf' ],
+		sourceMap: true
+	})
+]);
